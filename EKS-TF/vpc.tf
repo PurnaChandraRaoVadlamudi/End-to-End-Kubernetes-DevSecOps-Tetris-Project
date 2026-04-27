10296417ace024fa210240a2dbd859539dcb0c9f
PurnaChandra <126592017+PurnaChandraRaoVadlamudi@users.noreply.github.com>
2026-04-27 22:55:42 +0530

    Refactor VPC and related resources for EKS setup

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -1,34 +1,60 @@
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc-name]
+# Create VPC for EKS
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = var.vpc-name
   }
 }
 
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "tag:Name"
-    values = [var.igw-name]
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = var.igw-name
   }
 }
 
-data "aws_subnet" "subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet-name]
+# Create Subnet
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.1.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet-name
   }
 }
 
-data "aws_security_group" "sg-default" {
-  filter {
-    name   = "tag:Name"
-    values = [var.security-group-name]
+# Create Security Group
+resource "aws_security_group" "sg-default" {
+  vpc_id      = aws_vpc.vpc.id
+  description = "EKS Cluster Security Group"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security-group-name
   }
 }
 
 resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.1.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
@@ -38,10 +64,10 @@ resource "aws_subnet" "public-subnet2" {
 }
 
 resource "aws_route_table" "rt2" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
