73b33245c95def23955cffb8e76ac1f403f9c37b
PurnaChandra <126592017+PurnaChandraRaoVadlamudi@users.noreply.github.com>
2026-04-28 01:24:47 +0530

    Update EKS cluster configuration
    Enable public and private access to the EKS cluster endpoint and update version.

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -2,11 +2,16 @@ resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster-name
   role_arn = aws_iam_role.EKSClusterRole.arn
   vpc_config {
-    subnet_ids         = [aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
-    security_group_ids = [aws_security_group.sg-default.id]
+    subnet_ids              = [aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
+    security_group_ids      = [aws_security_group.sg-default.id]
+    endpoint_public_access  = true
+    endpoint_private_access = true
   }
 
-  version = 1.33
+  version = "1.29"
 
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_vpc.vpc
+  ]
 }
