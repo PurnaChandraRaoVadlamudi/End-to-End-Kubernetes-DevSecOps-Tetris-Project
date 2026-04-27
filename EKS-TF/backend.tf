d2db9d04ebd81416b89244af58b9ca6472638523
PurnaChandra <126592017+PurnaChandraRaoVadlamudi@users.noreply.github.com>
2026-04-27 22:44:03 +0530

    Fix formatting of S3 bucket name in backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "purna-s3-bucket-77 "
+    bucket       = "purna-s3-bucket-77"
     region       = "ap-south-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
